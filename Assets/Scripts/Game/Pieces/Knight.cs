af609e1e6a93647249a6fbb94eb3cf88a09ec70d
mcprisk <mcprisk@umich.edu>
2021-03-22 11:41:00 -0400

    Added Knight Functionality sent by Jennifer with slight modifications for jumping between walls

--- Assets/Scripts/Game/Pieces/Knight.cs
+++ Assets/Scripts/Game/Pieces/Knight.cs
@@ -7,14 +7,145 @@ public class Knight : Piece
     public override List<Vector3Int> SelectAvaliableSquares()
     {
         avaliableMoves.Clear();
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 0, 1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 0, 0));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 0, 1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 0, -1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 0, -1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 0, 1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 0, -1));
-        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 0, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 0, 2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 0, -2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 0, 2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 0, -2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(2, 0, 1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(2, 0, -1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-2, 0, 1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-2, 0, -1));
+
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 2, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -2, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 2, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -2, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(2, 1, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(2, -1, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-2, 1, 0));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(-2, -1, 0));
+
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 1, 2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 1, -2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, -1, 2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, -1, -2));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 2, 1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, 2, -1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, -2, 1));
+        avaliableMoves.Add(occupiedSquare + new Vector3Int(0, -2, -1));
+
+
+        // All moves forbidden on -Y:
+        if (occupiedSquare.y == 0 && occupiedSquare.x != 0 && occupiedSquare.z != 0
+            && occupiedSquare.x != 7 && occupiedSquare.z != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 1, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 1, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 2, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 2, -1));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, -1));
+        }
+        // All moves forbidden on +Y:
+        if (occupiedSquare.y == 7 && occupiedSquare.x != 0 && occupiedSquare.z != 0
+            && occupiedSquare.x != 7 && occupiedSquare.z != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, -2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, -2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, -1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, -1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -1, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -1, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -2, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -2, -1));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, -1));
+        }
+        // All moves forbidden on -X:
+        if (occupiedSquare.x == 0 && occupiedSquare.y != 0 && occupiedSquare.z != 0
+            && occupiedSquare.y != 7 && occupiedSquare.z != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 0, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 0, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 0, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 0, -1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, -2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, -1, 0));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, -1));
+        }
+        // All moves forbidden on +X:
+        if (occupiedSquare.x == 7 && occupiedSquare.y != 0 && occupiedSquare.z != 0
+            && occupiedSquare.y != 7 && occupiedSquare.z != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 0, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 0, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 0, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 0, -1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, -2, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 1, 0));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, -1, 0));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, -1));
+        }
+        // All moves forbidden on -Z:
+        if (occupiedSquare.z == 0 && occupiedSquare.x != 0 && occupiedSquare.y != 0
+            && occupiedSquare.x != 7 && occupiedSquare.y != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 0, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 0, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 0, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 0, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 1, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -1, 2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 2, 1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -2, 1));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, 1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, 1));
+        }
+        // All moves forbidden on +Z:
+        if (occupiedSquare.z == 7 && occupiedSquare.x != 0 && occupiedSquare.y != 0
+            && occupiedSquare.x != 7 && occupiedSquare.y != 7)
+        {
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(1, 0, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-1, 0, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(2, 0, -1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(-2, 0, -1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 1, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -1, -2));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, 2, -1));
+            avaliableMoves.Remove(occupiedSquare + new Vector3Int(0, -2, -1));
+
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, 1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, 1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(1, -1, -1));
+            avaliableMoves.Add(occupiedSquare + new Vector3Int(-1, -1, -1));
+        }
+
+        base.SelectAvaliableSquares();
+
         return avaliableMoves;
     }
 }
